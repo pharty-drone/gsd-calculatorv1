0b4006e8eadbe64729bc545c7055f3463b9ad6f0
pharty-drone <p.harty@skyscansurveys.co.uk>
2025-08-24 14:08:02 +0100

    Update .eslintrc.cjs

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,12 +1,10 @@
 module.exports = {
   env: { browser: true, es2021: true },
   extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: 'detect' } },
+  rules: {
+    'react/prop-types': 'off',                 // <-- disable PropTypes requirement
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
   },
-  settings: {
-    react: { version: 'detect' },
-  },
-  rules: {},
 };
