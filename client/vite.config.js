d454337af2dc9a1a2d182c49efc58c08e8485055
pharty-drone <p.harty@skyscansurveys.co.uk>
2025-08-24 15:31:26 +0100

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,4 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
 export default defineConfig({
-+  base: '/gsd-calculatorv1/',   // IMPORTANT: repo name
-  server: {
-    port: 5173,
-    proxy: { '/api': 'http://localhost:5000' },
-  },
-  plugins: [react()],
+  base: '/gsd-calculatorv1/', // repo name
+  // …
 });
